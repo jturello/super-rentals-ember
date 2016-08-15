85324255a91be87b23d54ea0efa28150ced5167d
James Turello <jturello@yahoo.com>
2016-08-15 16:35:28 -0700

    add delete rental functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,5 +4,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
+  },
+
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
