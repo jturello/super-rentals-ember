0fbfea44ac042ea84f2e04894eb6f676bc2a4008
James Turello <jturello@yahoo.com>
2016-08-16 13:34:45 -0700

    add update-rental component

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
     save1() {
       var params = {
         owner: this.get('owner') ? this.get('owner') : "",
-        city: this.get('city') ? this.get('city') : ""
+        city: this.get('city') ? this.get('city') : "",
         type: this.get('type') ? this.get('type') : "",
         image: this.get('image' ? this.get('image') : null),
         bedrooms: this.get('bedrooms' ? this.get('bedrooms') : ""),
