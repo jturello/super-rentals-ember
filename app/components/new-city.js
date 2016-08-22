17633a67ca804817a7c82d48ebb481f93dc7c332
James Turello <jturello@yahoo.com>
2016-08-22 12:15:59 -0700

    add city route and link city and rental models

--- app/components/new-city.js
+++ app/components/new-city.js
@@ -7,13 +7,13 @@ export default Ember.Component.extend({
       this.set('addNewCity', true);
     },
 
-    save() {
+    saveCity() {
       var params = {
         name: this.get('name') ? this.get('name') : "",
         country: this.get('country') ? this.get('country') : ""
       };
       this.set('addNewCity', false);
-      this.sendAction('save', params);
+      this.sendAction('saveCity', params);
     }
   }
 });
